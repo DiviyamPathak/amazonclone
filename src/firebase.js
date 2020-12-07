7d254a7cf6ac30fbea18ce75bc38fc27574849ea
unknown <patdiviyam@gmail.com>
2020-12-07 22:05:15 +0530

    react

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0tTKCbsWHhFuX69JrIjAtKlofXqr-l94",
+    authDomain: "fir-6f6e5.firebaseapp.com",
+    projectId: "fir-6f6e5",
+    storageBucket: "fir-6f6e5.appspot.com",
+    messagingSenderId: "923069665295",
+    appId: "1:923069665295:web:c7773ac41b4c2e0fb8937a"
+  };
